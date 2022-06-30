951163fec0dbd6ae27ba86125a4ebbb3068f3b28
David <david.williamx@gmail.com>
2022-06-29 22:25:11 -0300

    Adiciona cor vermelha quando número negativo

--- DIO/assets/js/script.js
+++ DIO/assets/js/script.js
@@ -4,9 +4,31 @@ var currentNumber = 0;
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0) {
+        currentNumberWrapper.style.color = 'red';
+    } else {
+        currentNumberWrapper.style.color = 'black';
+    }
 }
 
 function decrement() {
-    currentNumber = currentNumber -1;
+    currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0) {
+        currentNumberWrapper.style.color = 'red';
+    } else {
+        currentNumberWrapper.style.color = 'black';
+    }
 }
+
+// document.getElementById('btn-decrement').addEventListener("click", clicou);
+// document.getElementById('btn-increment').addEventListener("click", clicou);
+
+// function clicou() {
+//     if (document.getElementById('btn-decrement')) {
+//         console
+//     } else {
+        
+//     }
+//     console.log("Clicou!!!");
+// }
